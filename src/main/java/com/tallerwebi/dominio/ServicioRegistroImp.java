add60de3e233268e9dfef19c73238fe089634147
Tomi <tom.diuorno99@gmail.com>
2026-05-18 11:48:29 -0300

    Correccion de Tests, Separacion de responsabilidades en controladores

---
+++ src/main/java/com/tallerwebi/dominio/ServicioRegistroImp.java
@@ -0,0 +1,29 @@
+package com.tallerwebi.dominio;
+
+import com.tallerwebi.dominio.excepcion.UsuarioExistente;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("servicioRegistro")
+@Transactional
+public class ServicioRegistroImp implements ServicioRegistro {
+
+  private RepositorioUsuario repositorioUsuario;
+
+  @Autowired
+  public ServicioRegistroImp(RepositorioUsuario repositorioUsuario) {
+    this.repositorioUsuario = repositorioUsuario;
+  }
+
+  @Override
+  public void registrar(Usuario usuario) throws UsuarioExistente {
+    Usuario usuarioEncontrado = repositorioUsuario.buscarPorEmail(
+      usuario.getEmail()
+    );
+    if (usuarioEncontrado != null) {
+      throw new UsuarioExistente();
+    }
+    repositorioUsuario.guardar(usuario);
+  }
+}
---
+++ src/test/java/com/tallerwebi/dominio/ServicioRegistroTest.java
@@ -0,0 +1,51 @@
+package com.tallerwebi.dominio;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.tallerwebi.dominio.excepcion.UsuarioExistente;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ServicioRegistroTest {
+
+  private ServicioRegistro servicioRegistro;
+  private RepositorioUsuario repositorioUsuarioMock;
+
+  @BeforeEach
+  public void init() {
+    this.repositorioUsuarioMock = mock(RepositorioUsuario.class);
+    this.servicioRegistro = new ServicioRegistroImp(this.repositorioUsuarioMock);
+  }
+
+  @Test
+  public void registrarUsuarioSiNoExisteDeberiaGuardarlo() throws UsuarioExistente {
+    // preparacion
+    Usuario usuario = new Usuario();
+    usuario.setEmail("nuevo@test.com");
+    when(this.repositorioUsuarioMock.buscarPorEmail(usuario.getEmail()))
+      .thenReturn(null);
+
+    // ejecucion
+    this.servicioRegistro.registrar(usuario);
+
+    // validacion
+    verify(this.repositorioUsuarioMock, times(1)).guardar(usuario);
+  }
+
+  @Test
+  public void registrarUsuarioSiExisteDeberiaLanzarExcepcion() {
+    // preparacion
+    Usuario usuario = new Usuario();
+    usuario.setEmail("existe@test.com");
+    when(this.repositorioUsuarioMock.buscarPorEmail(usuario.getEmail()))
+      .thenReturn(new Usuario());
+
+    // ejecucion y validacion
+    assertThrows(UsuarioExistente.class, () -> this.servicioRegistro.registrar(usuario));
+    verify(this.repositorioUsuarioMock, times(0)).guardar(usuario);
+  }
+}
