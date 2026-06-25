53b469833d1f379a229935cc25c379ac96779ab0
Juampi <juanpablopfeiffer@gmail.com>
2026-06-25 19:06:53 -0300

    fix de dto publicacionDTO

--- src/main/java/com/tallerwebi/presentacion/DTO/PublicacionDTO.java
+++ src/main/java/com/tallerwebi/presentacion/DTO/PublicacionDTO.java
@@ -1,6 +1,5 @@
 package com.tallerwebi.presentacion.DTO;
 
-<<<<<<< HEAD
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,29 +7,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-=======
->>>>>>> f20d2fa78752f27cde6ba7ccf2dbc3e735c6e51a
 public class PublicacionDTO {
 
   private String titulo;
   private String contenido;
-<<<<<<< HEAD
-=======
-
-  public String getTitulo() {
-    return titulo;
-  }
-
-  public void setTitulo(String titulo) {
-    this.titulo = titulo;
-  }
-
-  public String getContenido() {
-    return contenido;
-  }
-
-  public void setContenido(String contenido) {
-    this.contenido = contenido;
-  }
->>>>>>> f20d2fa78752f27cde6ba7ccf2dbc3e735c6e51a
 }
