8dd6a2f9fd6e091ab44fc1d98b31ca037f2faa40
Juampi <juanpablopfeiffer@gmail.com>
2026-06-25 19:11:58 -0300

    fixes de codigo por violaciones de codigo

--- src/main/java/com/tallerwebi/dominio/entidades/Comentario.java
+++ src/main/java/com/tallerwebi/dominio/entidades/Comentario.java
@@ -7,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-<<<<<<< HEAD
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,9 +14,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
-=======
-
->>>>>>> f20d2fa78752f27cde6ba7ccf2dbc3e735c6e51a
 @Entity
 public class Comentario {
 
@@ -35,47 +31,4 @@ public class Comentario {
   @ManyToOne
   @JoinColumn(name = "publicacion_id")
   private Publicacion publicacion;
-<<<<<<< HEAD
-=======
-
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getContenido() {
-    return contenido;
-  }
-
-  public void setContenido(String contenido) {
-    this.contenido = contenido;
-  }
-
-  public LocalDateTime getFechaCreacion() {
-    return fechaCreacion;
-  }
-
-  public void setFechaCreacion(LocalDateTime fechaCreacion) {
-    this.fechaCreacion = fechaCreacion;
-  }
-
-  public Usuario getAutor() {
-    return autor;
-  }
-
-  public void setAutor(Usuario autor) {
-    this.autor = autor;
-  }
-
-  public Publicacion getPublicacion() {
-    return publicacion;
-  }
-
-  public void setPublicacion(Publicacion publicacion) {
-    this.publicacion = publicacion;
-  }
->>>>>>> f20d2fa78752f27cde6ba7ccf2dbc3e735c6e51a
 }
